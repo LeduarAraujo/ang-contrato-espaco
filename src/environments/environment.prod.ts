008d872b624476c9fa4740fdb22edda6f9baf699
LeduarAraujo <leduar.araujo@hotmail.com>
2025-09-28 03:38:16 -0300

    Desfazendo ajuste de url.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://42c88ed1-1083-4c7c-8bff-42de038165e9-00-1i94r1ycmvy9s.picard.replit.dev:8080/api'
+  apiUrl: 'http://localhost:8080/api'
 };
 
